5a7bb02b3325daea5de31db7cc11f78744f8b3a6
Ji Hyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-02-15 00:15:03 +0900

    [level 1] Title: 직사각형 별찍기, Time: 58.57 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,12 +1,7 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
-    let a = Number(n[0]), b = Number(n[1]);
-    a = '*'.repeat(a)
-    for (let i = 0; i < b; i++) {
-        i * console.log(a);
-    }
-    
-    
-    
+    const a = Number(n[0]), b = Number(n[1]);
+    console.log(a);
+    console.log(b);
 });
