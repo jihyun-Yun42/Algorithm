eed19a6f79e410a23df227649d1fe920e79267f4
Ji Hyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-02-22 10:27:35 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,3 +1,3 @@
 function solution(order) {
-    return Array.from(String(order)).reduce((pre, cur) => pre + (cur == 3 || cur == 6 || cur == 9? 1 : 0),0)
+    return Array.from(String(order)).reduce((acc, cur) => acc + (Number(cur) % 3 === 0 && Number(cur) !== 0 ? 1 : 0) ,0)
 }
