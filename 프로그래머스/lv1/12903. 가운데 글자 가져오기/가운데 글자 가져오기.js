c13e0268098508adef2e347dc8cd3c0ec70599ce
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-06-20 17:33:34 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,3 @@
 function solution(s) {
-    // Array.from(s)
-    // console.log(Array.from(s)[s.length / 2])
     return Number.isInteger(s.length / 2) ? s[s.length / 2 -1] + s[s.length / 2] : s[Math.floor(s.length / 2)]
 }
