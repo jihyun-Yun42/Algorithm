1dc9b5bc7068bd810bbc079ecb450c23df01ac18
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-07-07 23:32:12 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,5 @@
 function solution(phone_number) {
-    let phone = phone_number.split('')
+    let phone = [...phone_number]
     phone.splice(0, phone_number.length-4, '*'.repeat(phone_number.length-4))
     return phone.join('')
 }
