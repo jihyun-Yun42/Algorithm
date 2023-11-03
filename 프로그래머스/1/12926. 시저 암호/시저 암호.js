042046d1790b81edc42095a96cf452b9c1c9e873
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-11-03 12:36:31 +0900

    [level 1] Title: 시저 암호, Time: 4.42 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+    const upperStr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const lowerStr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    const targetArr = s.split('')
+    const newArr = targetArr.map((item) => {
+        if(item === ' ') {
+            return ' '
+        } else if(item.match(/[a-z]/)) {
+            const itemIndex = lowerStr.findIndex(str => str === item)
+            return lowerStr[itemIndex + n]
+        } else {
+            const itemIndex = upperStr.findIndex(str => str === item)
+            return upperStr[itemIndex + n]
+        }
+    })
+    return newArr.join('')
+}
