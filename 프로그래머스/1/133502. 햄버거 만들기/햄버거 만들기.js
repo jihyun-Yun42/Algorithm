db9a1d00c2902952227aa5a940bb432749c286d9
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-11-16 00:04:49 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,20 +1,15 @@
 function solution(ingredient) {
     let count = 0;
-    let stack = [];
-
-    for (let i = 0; i < ingredient.length; ++i) {
-        stack.push(ingredient[i]);
-
-        if (stack.length < 4) continue;
-
-        if (stack[stack.length - 4] === 1
-            && stack[stack.length - 3] === 2
-            && stack[stack.length - 2] === 3
-            && stack[stack.length - 1] === 1) {
-            for (let j = 0; j < 4; ++j) {
-                stack.pop();
-            }
-            count++;   
+    let arr = []
+    for(let i = 0; i < ingredient.length; i++) {
+        arr.push(ingredient[i])
+        
+        if(arr.at(-4) === 1
+            && arr.at(-3) === 2
+            && arr.at(-2) === 3
+            && arr.at(-1) === 1) {
+            arr.splice(arr.length -4, 4)
+            count++
         }
     }
 
