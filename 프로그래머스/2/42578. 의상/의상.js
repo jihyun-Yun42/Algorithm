ccfeac14e58de4034334735d1f8a11050a0c834a
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2023-11-22 21:50:29 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,9 +1,8 @@
 function solution(clothes) {
     const clothesObj = {}
     clothes.forEach(([value, key]) => {
-        if(key in clothesObj) clothesObj[key].push(value)
-        else clothesObj[key] = [value]
+        clothesObj[key] = (clothesObj[key] || 0) + 1
     })
 
-    return Object.values(clothesObj).reduce((acc, pre) => acc * (pre.length + 1), 1) -1
+    return Object.values(clothesObj).reduce((acc, pre) => acc * (pre + 1), 1) -1
 }
