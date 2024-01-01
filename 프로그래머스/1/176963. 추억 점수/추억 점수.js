c305c2804b8d92628ab336ac0516109396b978f6
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2024-01-01 23:26:29 +0900

    [level 1] Title: 추억 점수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,8 +1,3 @@
 function solution(name, yearning, photo) {
-    const nameObj = {}
-    name.map((name, index) => nameObj[name] = yearning[index])
-
-    return photo.map((item, index) => {
-        return item.reduce((acc, pre) => acc +(name.includes(pre) ? nameObj[pre] : 0),0)      
-    })
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
 }
