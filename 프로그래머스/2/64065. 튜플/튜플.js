2dd65618bbfc71c53ee85e922a033508cf940f6f
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2024-04-28 21:16:00 +0900

    [level 2] Title: 튜플, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    const regExp = /[\{\}]/g;
-    const newArr = s.replaceAll(regExp,'').split(',')
-    
-    const obj = {}
-    newArr.forEach(item => obj[item] = (obj[item] || 0) + 1)
-    
-    const sortObj = Object.entries(obj).sort((a, b) => b[1] - a[1])
-    return sortObj.map(([key, value]) => Number(key))
+    let answer = [];
+    let tuples = s.slice(2, -2).split(/},{/g).sort((a, b) => a.length - b.length);
+
+    tuples.forEach(v => {
+        let tuple = v.split(',');
+        answer.push(tuple.find(e => !answer.includes(e)));
+    });
+
+    return answer.map(e => Number(e));
 }
