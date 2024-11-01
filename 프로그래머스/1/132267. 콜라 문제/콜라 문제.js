137bc00ec5b2d50c8c7de617074b7c4694c2733d
jihyun Yun <122543836+jihyun-Yun42@users.noreply.github.com>
2024-11-01 22:58:52 +0900

    [level 1] Title: 콜라 문제, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,14 +1,9 @@
 function solution(a, b, n) {
-    let empties = n
-    let getCoke = 0
-    while(empties >= a) {
-        if(Number.isInteger(empties / a)) {
-            getCoke = getCoke + empties / a * b
-            empties = empties / a * b
-        } else {
-            getCoke = getCoke + parseInt(empties / a) * b
-            empties = parseInt(empties / a) * b + empties % a
-        }
+    var answer = 0;
+    
+    while(n >= a){
+        answer += Math.floor(n/a)*b;
+        n = n%a + Math.floor(n/a)*b;
     }
-    return getCoke
+    return answer;
 }
